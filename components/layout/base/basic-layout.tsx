004484446a0ecea26930f0cdc2ef19f16f3878a8
Albarra Erdanto <albarraerdanto@gmail.com>
2021-09-27 19:20:21 +0700

    create basic-layout

--- components/layout/base/basic-layout.tsx
+++ components/layout/base/basic-layout.tsx
@@ -1,12 +1,14 @@
 import MainHeader from "../header/main-header"
+import BottomNavbar from "../navbar/bottom-navbar"
 
 const BasicLayout: React.FC = ({ children }) => {
   return (
     <>
       <MainHeader />
-      <main>
+      <main className="pb-16">
         { children }
       </main>
+      <BottomNavbar />
     </>
   )
 }
