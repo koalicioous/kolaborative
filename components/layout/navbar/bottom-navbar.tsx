9369d752337b2d9bf0a97c70ba0023b5c3032b79
Albarra Erdanto <albarraerdanto@gmail.com>
2021-09-29 07:39:51 +0700

    navbar modularization

--- components/layout/navbar/bottom-navbar.tsx
+++ components/layout/navbar/bottom-navbar.tsx
@@ -1,59 +1,20 @@
 import Link from 'next/link'
-
-interface Menu {
-  name: string,
-  icon: JSX.Element,
-  href: string
-}
+import { useRouter } from 'next/router'
+import { menus } from '../../../data/layout/navbar'
 
 const BottomNavbar: React.FC = () => {
-  const menus: Menu[] = [
-    {
-      name: 'Home',
-      icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-        </svg>
-      ),
-      href: '/'
-    },
-    {
-      name: 'Saved',
-      icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
-        </svg>
-      ),
-      href: '/saved'
-    },
-    {
-      name: 'My Projects',
-      icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
-        </svg>
-      ),
-      href: '/myprojects'
-    },
-    {
-      name: 'Profile',
-      icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-        </svg>
-      ),
-      href: '/myprofile'
-    }
-  ]
+  const router = useRouter()
 
   return (
     <nav className="fixed bottom-0 inset-x-0 z-10 bg-white w-full shadow-xl border-t">
       <ul className="max-w-lg m-auto flex">
         {menus.map(menu => {
+          const isActive = menu.href === router.asPath
+
           return (
             <li key={menu.name} className="flex-1">
               <Link href={menu.href}>
-                <a className="p-2 text-xs text-indigo-700 text-center flex flex-col justify-center items-center">
+                <a className={`p-2 text-xs text-center flex flex-col justify-center items-center ${isActive ? 'text-indigo-700' : 'text-gray-600'}`}>
                   {menu.icon}
                   <p className="mt-1">
                     {menu.name}
