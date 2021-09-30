fdc501e0f717458241a848a2c1974ffdffd99979
Albarra Erdanto <albarraerdanto@gmail.com>
2021-09-30 18:51:20 +0700

    change to modulated icon

--- lib/layout/navbar.tsx
+++ lib/layout/navbar.tsx
@@ -1,3 +1,7 @@
+import BookmarkIcon from '../../components/ui/icons/bookmark-icon';
+import HomeIcon from '../../components/ui/icons/home-icon';
+import SparklesIcon from '../../components/ui/icons/sparkles-icon';
+import UserIcon from '../../components/ui/icons/user-icon';
 import { MODE_OWNER, MODE_TALENT } from '../../constants/global';
 
 export interface Menu {
@@ -10,38 +14,22 @@ export interface Menu {
 export const menus: Menu[] = [
   {
     name: 'Home',
-    icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-      </svg>
-    ),
+    icon: <HomeIcon className="h-5 w-5" />,
     href: ['/', `/?mode=${MODE_TALENT}`, `/?mode=${MODE_OWNER}`],
   },
   {
     name: 'Saved',
-    icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
-      </svg>
-    ),
+    icon: <BookmarkIcon className="h-5 w-5" />,
     href: '/saved',
   },
   {
     name: 'My Projects',
-    icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
-      </svg>
-    ),
+    icon: <SparklesIcon className="h-5 w-5" />,
     href: '/myprojects',
   },
   {
     name: 'Profile',
-    icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-      </svg>
-    ),
+    icon: <UserIcon className="h-5 w-5" />,
     href: '/myprofile',
   },
 ];
